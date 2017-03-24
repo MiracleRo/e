6525f96d71626aa6235be795bd4ea79007474fda
MiracleRo <741556866@qq.com>
2017-03-24 09:46:21 +0800

    完成header组件

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -32,19 +32,80 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div class='detail' v-show='detailShow'>
-      <div class="detail-wrapper clear-fix">
-        <div class="detail-main">
-          <h1 class="name">
-            {{seller.name}}
-          </h1>
-          <star :size="48" :score="seller.score"></star>
+    <transition name="fade">
+      <div class='detail' v-show='detailShow'>
+        <div class="detail-wrapper clear-fix">
+          <div class="detail-main">
+            <h1 class="name">
+              {{seller.name}}
+            </h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul  v-if="seller.supports" class="support">
+              <li class="class-item" v-for="(item, index) in seller.supports">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <div class="bulletin-text">
+                {{seller.bulletin}}
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
+        </div>
+      </div trans>
+    </transition>
+    <!--<div class='detail' v-show='detailShow' transition="fade">-->
+      <!--<div class="detail-wrapper clear-fix">-->
+        <!--<div class="detail-main">-->
+          <!--<h1 class="name">-->
+            <!--{{seller.name}}-->
+          <!--</h1>-->
+          <!--<div class="star-wrapper">-->
+            <!--<star :size="48" :score="seller.score"></star>-->
+          <!--</div>-->
+          <!--<div class="title">-->
+            <!--<div class="line"></div>-->
+            <!--<div class="text">优惠信息</div>-->
+            <!--<div class="line"></div>-->
+          <!--</div>-->
+          <!--<ul  v-if="seller.supports" class="support">-->
+            <!--<li class="class-item" v-for="(item, index) in seller.supports">-->
+              <!--<span class="icon" :class="classMap[seller.supports[index].type]"></span>-->
+              <!--<span class="text">{{seller.supports[index].description}}</span>-->
+            <!--</li>-->
+          <!--</ul>-->
+          <!--<div class="title">-->
+            <!--<div class="line"></div>-->
+            <!--<div class="text">商家公告</div>-->
+            <!--<div class="line"></div>-->
+          <!--</div>-->
+          <!--<div class="bulletin">-->
+            <!--<div class="bulletin-text">-->
+              <!--{{seller.bulletin}}-->
+            <!--</div>-->
+          <!--</div>-->
+        <!--</div>-->
+      <!--</div>-->
+      <!--<div class="detail-close" @click="hideDetail">-->
+        <!--<i class="icon-close"></i>-->
+      <!--</div>-->
+    <!--</div trans>-->
   </div>
 </template>
 
@@ -66,6 +127,9 @@
   methods: {
     showDetail() {
       this.detailShow = true;
+    },
+    hideDetail() {
+      this.detailShow = false;
     }
   },
 
@@ -202,7 +266,13 @@
       height: 100%
       width: 100%
       overflow: auto
-      background-color: rgba(7, 17, 27, 0.8)
+      background: rgba(7, 17, 27, 0.8)
+      backdrop-filter: blur(10px)
+      &.fade-enter-active, &.fade-leave-active
+        transition: opacity .5s
+        background: rgba(7, 17, 27, 0.8)
+      &.fade-enter, &.fade-leave-active
+        opacity: 0
       .detail-wrapper
         min-height: 100%
         width: 100%
@@ -215,6 +285,62 @@
             line-height: 16px
             font-weight: 700
             text-align: center
+          .star-wrapper
+            text-align: center
+            margin-top: 16px
+            padding: 2px 0
+          .title
+            display: flex
+            width: 80%
+            margin: 28px auto 24px auto
+            .line
+              flex: 1
+              position: relative
+              top: -6px
+              border-bottom: 1px solid rgba(255,255,255,0.2)
+            .text
+              padding: 0 12px
+              font-size: 14px
+              font-weight: 700
+          .support
+            width: 80%
+            margin: 0 auto
+            .class-item
+              padding: 0 12px
+              margin-bottom: 12px
+              font-size: 0
+              &:last-child
+                margin-bottom: 0
+              .icon
+                display: inline-block
+                width: 16px
+                height: 16px
+                vertical-align: top
+                margin-right: 6px
+                background-size: 16px 16px
+                background-repeat: no-repeat
+                &.decrease
+                  bg-image('decrease_2')
+                &.discount
+                  bg-image('discount_2')
+                &.guarantee
+                  bg-image('guarantee_2')
+                &.invoice
+                  bg-image('invoice_2')
+                &.special
+                  bg-image('special_2')
+              .text
+                font-size: 12px
+                font-weight: 200
+                line-height: 16px
+          .bulletin
+            width: 80%
+            margin: 0 auto
+            .bulletin-text
+              font-size: 14px
+              font-weight: 200
+              line-height: 24px
+              padding: 0 12px
       .detail-close
         position: relative
         width: 32px
